1274eaa56a0580067698eb8798788a86a623ccf3
b1ackitty <jieunj0301@gmail.com>
2025-08-24 20:05:50 +0900

    [add] 2주차 과제 - 자료 준비

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,32 @@
 import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo/app'
 
 export default function App() {
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
+    <LearnSection
+      title="React 과제"
+      showTitle
+      className="m-8 flex flex-col gap-2"
+    >
+      <ul>
+        <li>
+          <a
+            href="https://github.com/b1ackitty/react-js-vite"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>1주차 과제</strong> - 리액트 프로젝트 템플릿 만들기
+          </a>
+        </li>
+        <li>
+          <a
+            href="./src/components/week2-homework/index.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <strong>2주차 과제</strong> - 쿠팡 로그인 UI 개선
+          </a>
+        </li>
+      </ul>
     </LearnSection>
   )
 }
